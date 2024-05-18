b2238088607e7b4b3f1031615e6c73fad113b836
Hector Mauricio Muñoz Ordoñez <dev.mauricio.munoz@gmail.com>
2024-05-18 08:52:47 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Servidor ejecutandose en http://localhost:3000/");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor ejecutándose en http://localhost:3000/");
 });
 
 // Dividir el código
